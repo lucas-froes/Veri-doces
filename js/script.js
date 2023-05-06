1e8734fff2d311b863b78e12783b9466493e423e
Lucas <lucas-froes@hotmail.com>
2023-05-06 12:19:51 -0300

    Slide funcionando

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 let arrowConteudo = document.querySelectorAll('.cursos-box-area');
+let click = 0
 
-function clickCursos(event){
-   console.log (event.currentTarget.children[1].classList.toggle('on-conteudo')); 
+function clickCursos(event){ 
 
-   if(event.currentTarget.children[1].classList.toggle('on-conteudo')== false){
-    console.log(event.currentTarget.children[1]);
-   }else{
+  if(event.currentTarget.children[1].classList[2]){
+    event.currentTarget.children[1].classList.remove('on-conteudo');
+  }
+  else{
+    event.currentTarget.children[1].classList.add('on-conteudo');
+  }
+  
+  arrowConteudo.forEach((click, i) =>{
 
-   }
-   
+    if(event.currentTarget != click){
+       click.children[1].classList.remove('on-conteudo');
+    }
+  })
 }
+
 arrowConteudo.forEach((cursos) =>{
     cursos.addEventListener('click',clickCursos);
 })
